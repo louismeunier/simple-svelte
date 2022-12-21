da629c4837f551d00a045009b709778c11ca7bf7
Louis Meunier <louismeunier123@gmail.com>
2022-12-21 12:27:34 -0500

    Init repo

---
+++ src/App.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+  // :)
+</script>
+
+<main>
+  <h1 class="title">
+    a super simple <i class="vite">vite</i> + <i class="svelte">svelte</i> template
+  </h1>
+</main>
+
+<style>
+  main {
+    text-align: center;
+    max-width: 240px;
+    margin: 0 auto;
+    display: grid;
+    place-items: center;
+    height: 100vh;
+  }
+
+  i {
+    text-decoration: dashed underline;
+  }
+
+  .vite {
+    color: rgb(89, 0, 89);
+  }
+
+  .svelte {
+    color: rgb(255, 62, 0);
+  }
+</style>
